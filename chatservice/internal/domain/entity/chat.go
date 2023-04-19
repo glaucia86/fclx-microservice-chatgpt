6e01aafb1889472042cd7ed1adce67a97950f5aa
glaucia86 <glaucia_lemos86@hotmail.com>
2023-04-19 18:53:16 -0300

    chore: develop 'chat' and 'message' structure

--- chatservice/internal/domain/entity/chat.go
+++ chatservice/internal/domain/entity/chat.go
@@ -6,3 +6,18 @@
  */
 
 package entity
+
+type ChatConfig struct {
+	Model *Model
+}
+
+type Chat struct {
+	ID                   string
+	UserID               string
+	InitialSystemMessage *Message
+	Messages             []*Message
+	ErasedMessages       []*Message
+	Status               string
+	TokenUsage           int
+	Config               *ChatConfig
+}
---
+++ chatservice/internal/domain/entity/message.go
@@ -0,0 +1,10 @@
+/**
+ * file: internal/domain/entity/message.go
+ * date: 04/18/2023
+ * description: file responsible for the message entity
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+package entity
+
+type Message struct{}
