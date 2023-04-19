c3a3765b1c798c98475303c85b55b13c4dfd7096
glaucia86 <glaucia_lemos86@hotmail.com>
2023-04-19 18:52:49 -0300

    fix: deleted unecessary file

--- chatservice/internal/domain/entity/entity.go
+++
@@ -1,8 +0,0 @@
-/**
- * file: internal/domain/entity/entity.go
- * date: 04/18/2023
- * description: file responsible for the creation of the entity of the application
- * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
- */
-
-package entity
